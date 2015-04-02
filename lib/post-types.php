b14a0d1b68a97f9eb020e42aaa5195bd97372101
Patrick Best <patrickbest@patrickbest.com>
2015-04-02 11:57:04 -0600

    comment post types

--- lib/post-types.php
+++ lib/post-types.php
@@ -13,51 +13,7 @@ function add_menu_icons_styles(){
 }
 add_action( 'admin_head', 'add_menu_icons_styles' );
 
-
-//Register Custom Post Types
-add_action( 'init', 'register_cpt_projects' );
-
-function register_cpt_projects() {
-
-    $labels = array(
-        'name' => _x( 'Projects', 'projects' ),
-        'singular_name' => _x( 'Project', 'projects' ),
-        'add_new' => _x( 'Add New', 'projects' ),
-        'add_new_item' => _x( 'Add New Project', 'projects' ),
-        'edit_item' => _x( 'Edit Project', 'projects' ),
-        'new_item' => _x( 'New Project', 'projects' ),
-        'view_item' => _x( 'View Project', 'projects' ),
-        'search_items' => _x( 'Search Projects', 'projects' ),
-        'not_found' => _x( 'No projects found', 'projects' ),
-        'not_found_in_trash' => _x( 'No projects found in Trash', 'projects' ),
-        'parent_item_colon' => _x( 'Parent Project:', 'projects' ),
-        'menu_name' => _x( 'Projects', 'projects' ),
-    );
-
-    $args = array(
-        'labels' => $labels,
-        'hierarchical' => false,
-
-        'supports' => array( 'title', 'editor', 'thumbnail' ),
-        'taxonomies' => array( 'category' ),
-        'public' => true,
-        'show_ui' => true,
-        'show_in_menu' => true,
-        'menu_position' => 5,
-
-        'show_in_nav_menus' => true,
-        'publicly_queryable' => true,
-        'exclude_from_search' => false,
-        'has_archive' => true,
-        'query_var' => true,
-        'can_export' => true,
-        'rewrite' => true,
-        'capability_type' => 'post'
-    );
-
-    register_post_type( 'projects', $args );
-}
-
+// REGISTER CLIENTS
 add_action( 'init', 'register_cpt_clients' );
 
 function register_cpt_clients() {
@@ -99,4 +55,48 @@ function register_cpt_clients() {
     );
 
     register_post_type( 'clients', $args );
+}
+
+// ARCHIVE POST TYPE FOR ADMIN REASONS
+add_action( 'init', 'register_cpt_projects' );
+
+function register_cpt_projects() {
+
+    $labels = array(
+        'name' => _x( 'Projects', 'projects' ),
+        'singular_name' => _x( 'Project', 'projects' ),
+        'add_new' => _x( 'Add New', 'projects' ),
+        'add_new_item' => _x( 'Add New Project', 'projects' ),
+        'edit_item' => _x( 'Edit Project', 'projects' ),
+        'new_item' => _x( 'New Project', 'projects' ),
+        'view_item' => _x( 'View Project', 'projects' ),
+        'search_items' => _x( 'Search Projects', 'projects' ),
+        'not_found' => _x( 'No projects found', 'projects' ),
+        'not_found_in_trash' => _x( 'No projects found in Trash', 'projects' ),
+        'parent_item_colon' => _x( 'Parent Project:', 'projects' ),
+        'menu_name' => _x( 'Projects', 'projects' ),
+    );
+
+    $args = array(
+        'labels' => $labels,
+        'hierarchical' => false,
+
+        'supports' => array( 'title', 'editor', 'thumbnail' ),
+        'taxonomies' => array( 'category' ),
+        'public' => true,
+        'show_ui' => true,
+        'show_in_menu' => true,
+        'menu_position' => 5,
+
+        'show_in_nav_menus' => true,
+        'publicly_queryable' => true,
+        'exclude_from_search' => false,
+        'has_archive' => true,
+        'query_var' => true,
+        'can_export' => true,
+        'rewrite' => true,
+        'capability_type' => 'post'
+    );
+
+    register_post_type( 'projects', $args );
 }
